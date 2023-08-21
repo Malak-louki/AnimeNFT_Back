bdc3bbcb1d6b50e5c0d0381d1c96e04a42edb22a
Malak Bouaker <bouaker.malak@outlook.fr>
2023-08-21 09:49:14 +0200

    easyAdmin mis en place

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller\Admin;
 
+use EasyCorp\Bundle\EasyAdminBundle;
 use App\Entity\Adress;
 use App\Entity\Category;
 use App\Entity\Eth;
 use App\Entity\Image;
 use App\Entity\Nft;
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -19,23 +21,7 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
-
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
+        return $this->render('Admin/dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
@@ -48,10 +34,10 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Adresses', 'fas fa-list', Adress::class);
-        // yield MenuItem::linkToCrud('Catégories', 'fas fa-list', Category::class);
-        // yield MenuItem::linkToCrud("les cours de l'ETH", 'fas fa-list', Eth::class);
-        // yield MenuItem::linkToCrud('Les images NFT', 'fas fa-list', Image::class);
-        // yield MenuItem::linkToCrud('Les NFTs', 'fas fa-list', Nft::class);
-        // yield MenuItem::linkToCrud('Les utilisateurs', 'fas fa-list', User::class);
+        yield MenuItem::linkToCrud('Catégories', 'fas fa-list', Category::class);
+        yield MenuItem::linkToCrud("les cours de l'ETH", 'fas fa-list', Eth::class);
+        yield MenuItem::linkToCrud('Les images NFT', 'fas fa-list', Image::class);
+        yield MenuItem::linkToCrud('Les NFTs', 'fas fa-list', Nft::class);
+        yield MenuItem::linkToCrud('Les utilisateurs', 'fas fa-list', User::class);
     }
 }
